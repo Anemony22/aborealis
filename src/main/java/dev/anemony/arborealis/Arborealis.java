61ac48a5d4fecc76695b5102a2aefe41eac814f1
Ben C <bennyjc01@gmail.com>
2024-12-07 12:42:20 +0800

    Template renamings

--- src/main/java/dev/anemony/arborealis/Arborealis.java
+++ src/main/java/dev/anemony/arborealis/Arborealis.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package dev.anemony.arborealis;
 
 import org.slf4j.Logger;
 
@@ -35,8 +35,8 @@ import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(Arborealis.MODID)
+public class Arborealis
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "arborealis";
@@ -69,7 +69,7 @@ public class ExampleMod
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public ExampleMod(IEventBus modEventBus, ModContainer modContainer)
+    public Arborealis(IEventBus modEventBus, ModContainer modContainer)
     {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -82,7 +82,7 @@ public class ExampleMod
         CREATIVE_MODE_TABS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
+        // Note that this is necessary if and only if we want *this* class (Arborealis) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
